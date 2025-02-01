eeb071738245164c69439af42e81366a7d3f5cd2
larisaioana <vi.larisaioana@gmail.com>
2025-02-01 21:24:08 +0200

    rezolvare conflicte

---
+++ taskmaster-backend/src/main/java/com/taskmaster/taskmaster_backend/DeadlineReminderScheduler.java
@@ -0,0 +1,44 @@
+package com.taskmaster.taskmaster_backend;
+
+import com.taskmaster.taskmaster_backend.model.Task;
+import com.taskmaster.taskmaster_backend.repository.TaskRepository;
+import com.taskmaster.taskmaster_backend.service.EmailService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class DeadlineReminderScheduler {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+//    @Scheduled(cron = "0 * * * * ?")  //pentru fiecare minut
+    @Scheduled(cron = "0 0 9 * * ?")
+    @Transactional
+    public void sendDeadlineReminders() {
+        LocalDate today = LocalDate.now();
+        LocalDate upcomingDate = today.plusDays(1);
+
+        List<Task> upcomingTasks = taskRepository.findAll().stream()
+                .filter(task -> task.getDeadline() != null &&
+                        !task.getDeadline().isBefore(today) &&
+                        task.getDeadline().equals(upcomingDate) &&
+                        !Boolean.TRUE.equals(task.getCompleted()))
+                .toList();
+
+        for (Task task : upcomingTasks) {
+            String email = task.getUser().getEmail();
+            String subject = "Reminder: Deadline pentru task-ul tău!";
+            String body = "Task-ul tau, \"" + task.getTitle() + "\" are deadline pe " + task.getDeadline() + ".";
+
+            emailService.sendEmail(email, subject, body);
+        }
+    }
+}
