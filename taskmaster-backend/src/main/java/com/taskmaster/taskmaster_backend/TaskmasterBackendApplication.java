dbf58668d86a5859cb1293ec8bca2c522a81d7ef
sebastianMustatoiu <ioan-sebastian.mustatoiu@s.unibuc.ro>
2025-02-01 20:13:14 +0200

    Am adaugat sistem de trimitere notificari

--- taskmaster-backend/src/main/java/com/taskmaster/taskmaster_backend/TaskmasterBackendApplication.java
+++ taskmaster-backend/src/main/java/com/taskmaster/taskmaster_backend/TaskmasterBackendApplication.java
@@ -2,7 +2,9 @@ package com.taskmaster.taskmaster_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TaskmasterBackendApplication {
 	public static void main(String[] args) {
